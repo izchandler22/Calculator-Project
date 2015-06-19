e9460d95aec72169fcbb210ab4a6e158a392005f
Rachel Alexander <rachel.alexander198@aol.com>
2015-06-19 18:24:27 +0000

    calculator.rb

--- calculator.rb
+++ calculator.rb
@@ -1,11 +1,7 @@
 #HI YALL
 def addition(num1, num2)
-<<<<<<< HEAD
-   return num1 + num2 
-end
 
-def substraction(num1, num2)
-   puts num1-num2
+   return num1 + num2 
 end
 
 def division(num1, num2)
@@ -14,11 +10,8 @@ end
 
 puts division(9, 3)
 puts addition(3, 198)
-=======
-  
-end
 
 def substraction(num1, num2)
   return num1-num2
 end
->>>>>>> 23f810e568ededec622a856be896b604e41de73c
+
