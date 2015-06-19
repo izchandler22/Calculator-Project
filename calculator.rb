60e3a48926f8674eab6327ec264a5d5f2360675b
izchandler22 <isskisoc8@gmail.com>
2015-06-19 18:35:41 +0000

    deletion

--- calculator.rb
+++ calculator.rb
@@ -13,17 +13,13 @@ def division(num1, num2)
   return num1 / num2
 end
 
-<<<<<<< HEAD
-puts division(9, 3)
-puts addition(3, 198)
-=======
 def square_root(num1)
   return Math.sqrt(num1)
 end
 
 
 
->>>>>>> a804ffadde28ce2d0b736998432684a794ac85be
+
 
 def substraction(num1, num2)
   return num1-num2
@@ -31,6 +27,4 @@ end
 
 
 
-puts division(9, 3)
-puts addition(3, 198)
 
