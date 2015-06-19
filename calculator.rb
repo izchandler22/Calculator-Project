8d9c44910d98996549f0ef88d7edbf830b5d5cbc
izchandler22 <isskisoc8@gmail.com>
2015-06-19 18:12:11 +0000

    changed

--- calculator.rb
+++ calculator.rb
@@ -1,23 +1,15 @@
 #HI YALL
 def addition(num1, num2)
-<<<<<<< HEAD
-  
+   return num1 + num2 
 end
 
 def substraction(num1, num2)
    puts num1-num2
 end
-=======
-  return num1 + num2 
-end
-<<<<<<< HEAD
-puts addition(3, 198)
->>>>>>> 32398e62ed856870f7857d0977197a35f08c4532
-=======
-
 
 def division(num1, num2)
   return num1 / num2
 end
+
 puts division(9, 3)
->>>>>>> 7cd0de9c153d94d7f1e91f48e13f01517207dd73
+puts addition(3, 198)
