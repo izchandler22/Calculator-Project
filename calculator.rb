d3fc02f54c2e10c89376f3d1b0d24e8dfb5e0eef
izchandler22 <isskisoc8@gmail.com>
2015-06-19 18:31:13 +0000

    delete

--- calculator.rb
+++ calculator.rb
@@ -22,17 +22,6 @@ def square_root(num1)
 end
 
 
-puts square_root(4)
-=======
-end
 
-def division(num1, num2)
-  return num1 / num2
-end
-
-
-puts division(9, 3)
-puts addition(3, 198)
->>>>>>> 557ba0d2d0a68efbcf91570e02cd810fce66a877
 
 
