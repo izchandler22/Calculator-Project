557ba0d2d0a68efbcf91570e02cd810fce66a877
charhutch21 <charhutch21@gmail.com>
2015-06-19 18:26:37 +0000

    update

--- calculator.rb
+++ calculator.rb
@@ -6,14 +6,15 @@ end
 def substraction(num1, num2)
    puts num1-num2
 end
-
-def division(num1, num2)
-  return num1 / num2
-end
   
 def multiplucation(num1, num2)
   return num1 * num2
 end
+
+def division(num1, num2)
+  return num1 / num2
+end
+
 
 puts division(9, 3)
 puts addition(3, 198)
