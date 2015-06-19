4b8ac4ad4b2b2542a160ebae998865d2b9629ce5
izchandler22 <isskisoc8@gmail.com>
2015-06-19 18:29:52 +0000

    sqrt

--- calculator.rb
+++ calculator.rb
@@ -7,16 +7,20 @@ end
 def substraction(num1, num2)
    puts num1-num2
 end
-
-def division(num1, num2)
-  return num1 / num2
-end
   
 def multiplucation(num1, num2)
   return num1 * num2
 end
 
-puts division(9, 3)
-puts addition(3, 198)
+def division(num1, num2)
+  return num1 / num2
+end
+
+def square_root(num1)
+  return Math.sqrt(num1)
+end
+
+
+puts square_root(4)
 
 
