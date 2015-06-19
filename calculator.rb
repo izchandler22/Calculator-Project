cdfbe576e812defc23350b804011d626bbfcd445
izchandler22 <isskisoc8@gmail.com>
2015-06-19 18:25:30 +0000

    deletions

--- calculator.rb
+++ calculator.rb
@@ -10,20 +10,12 @@ end
 def division(num1, num2)
   return num1 / num2
 end
-
-puts division(9, 3)
-puts addition(3, 198)
   
-end
 def multiplucation(num1, num2)
   return num1 * num2
 end
 
-def division(num1, num2)
-  return num1 / num2
-end
 puts division(9, 3)
+puts addition(3, 198)
 
-def substraction(num1, num2)
-  return num1-num2
-end
+
